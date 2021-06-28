b3657791f4a5ccaa6f24355811597fafa303bd2d
JanCieslak <61777128+JanCieslak@users.noreply.github.com>
2021-06-28 13:58:46 +0200

    Init commit

---
+++ src/main/java/io/github/jancieslak/tba/TbaApplication.java
@@ -0,0 +1,13 @@
+package io.github.jancieslak.tba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TbaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/jancieslak/tba/TbaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jancieslak.tba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
